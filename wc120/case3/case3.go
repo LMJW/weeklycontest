17ba4245e500e169bb49cd1caaca08c2393163ee
LMJW <heysuperming@gmail.com>
2019-01-20 17:31:44 +1100

    case3

--- wc120/case3/case3.go
+++ wc120/case3/case3.go
@@ -7,61 +7,32 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func constructmaxtix(root *TreeNode) (map[int]int, [][2]int, int) {
-	var idx int
-
-	var nodemap = map[*TreeNode]int{}
-	var nodecoin = map[int]int{}
-	var distance [][2]int
-	var traver func(*TreeNode)
-
-	// traver the tree to get the distance map
-	traver = func(n *TreeNode) {
-		if _, ok := nodemap[n]; !ok {
-			nodemap[n] = idx
-			idx++
-			if n.Val > 0 {
-				nodecoin[nodemap[n]] = n.Val
-			}
-		}
-		if n.Left != nil {
-			nodemap[n.Left] = idx
-			idx++
-			distance = append(distance, [2]int{nodemap[n], nodemap[n.Left]})
-		}
-		if n.Right != nil {
-			nodemap[n.Right] = idx
-			idx++
-			distance = append(distance, [2]int{nodemap[n], nodemap[n.Right]})
-		}
-
-		if n.Left != nil {
-			traver(n.Left)
-		}
-		if n.Right != nil {
-			traver(n.Right)
-		}
+func abs(x int) int {
+	if x > 0 {
+		return x
 	}
-
-	return nodecoin, distance, idx
+	return -x
 }
 
 func distributeCoins(root *TreeNode) int {
-	coins, distances, nnodes := constructmaxtix(root)
+	var res int
+	var calcost func(n *TreeNode) int
 
-	var matrix = [][]int{}
-
-	for i := 0; i < nnodes; i++ {
-		var row = make([]int, nnodes)
-		matrix = append(matrix, row)
+	calcost = func(n *TreeNode) int {
+		var l, r int
+		if n.Left != nil {
+			l = calcost(n.Left)
+		}
+		if n.Right != nil {
+			r = calcost(n.Right)
+		}
+		var t = n.Val + l + r - 1
+		res += abs(t)
+		return t
 	}
+	_ = calcost(root)
 
-	for i, v := range distances {
-		var a, b = v[0], v[1]
-		matrix[a][b] = 1
-		matrix[b][a] = 1
-	}
-
+	return res
 }
 
 func main() {

c59f0a657c3fe68bb921d0d96f3aa137111dffe2
LMJW <heysuperming@gmail.com>
2019-02-11 07:57:00 +1100

    disjoint set solution

--- wc123/case2.cpp
+++ wc123/case2.cpp
@@ -8,25 +8,10 @@ using namespace std;
 struct disjointset {
     disjointset() {}
     unordered_map<int, int> ds;
-    unordered_set<int> ueq;
-    bool addequal(int v1, int v2) {
+    void addequal(int v1, int v2) {
         int p1 = parent(v1);
         int p2 = parent(v2);
-        if (ueq.find(p1) != ueq.end() && ueq.find(p2) != ueq.end()) {
-            return false;
-        }
         ds[p1] = p2;
-        return true;
-    }
-    bool add_unequal(int v1, int v2) {
-        int p1 = parent(v1);
-        int p2 = parent(v2);
-        if (p1 == p2) {
-            return false;
-        }
-        ueq.insert(p1);
-        ueq.insert(p2);
-        return true;
     }
     int parent(int v) {
         if (ds.find(v) == ds.end()) {
@@ -39,6 +24,14 @@ struct disjointset {
         }
         return ds[v];
     }
+    bool check_eq(int v1, int v2) {
+        int p1 = parent(v1);
+        int p2 = parent(v2);
+        if (p1 == p2) {
+            return true;
+        }
+        return false;
+    }
 };
 
 class Solution {
@@ -48,35 +41,38 @@ public:
         /// equality can be stored in a disjoint set
         /// How can i store a!=b, a!=c, a!=d?
         disjointset ds;
+        vector<string> eqs_not_equal;
 
         for (auto eq : equations) {
             int v1 = eq[0] - 'a';
             int v2 = eq[3] - 'a';
+
             bool relation = eq[1] == '=';
             // cout << v1 << " " << v2 << " " << relation << " \n";
-            if (!addtoset(ds, v1, v2, relation)) {
+            if (relation) {
+                addtoset(ds, v1, v2);
+            } else {
+                eqs_not_equal.push_back(eq);
+            }
+        }
+        for (auto eq : eqs_not_equal) {
+            int v1 = eq[0] - 'a';
+            int v2 = eq[3] - 'a';
+            if (ds.check_eq(v1, v2)) {
                 return false;
             }
         }
+
         return true;
     }
 
-    bool addtoset(disjointset& ds, int v1, int v2, bool rel) {
-        if (rel) {
-            if (!ds.addequal(v1, v2)) {
-                return false;
-            }
-        } else {
-            if (!ds.add_unequal(v1, v2)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    void addtoset(disjointset& ds, int v1, int v2) { ds.addequal(v1, v2); }
 };
 
 int main() {
-    auto a = vector<string>{"a==b", "c==b", "a==c"};
+    // auto a = vector<string>{"a!=b", "c!=d", "a==c"};
+    auto a = vector<string>{"a!=b", "c!=d", "a==c", "b==c"};
+    // auto a = vector<string>{"e==d", "e==a", "f!=d", "b!=c", "a==b"};
     auto x = Solution().equationsPossible(a);
     cout << x << endl;
 }

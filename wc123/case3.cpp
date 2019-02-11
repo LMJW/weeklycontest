9a87bf4259ebe4bf0fa058823398d4ff2768a9e6
LMJW <heysuperming@gmail.com>
2019-02-11 23:54:26 +1100

    solution. Notice the input size is 10^9, this usually means program will take more than few seconds to execute. So, for such a big input size, we should only consider a solution with log(x) or less.

--- wc123/case3.cpp
+++ wc123/case3.cpp
@@ -8,38 +8,16 @@ using namespace std;
 class Solution {
 public:
     int brokenCalc(int x, int y) {
-        if (x >= y) {
-            return x - y;
-        }
-        unordered_set<int> hist;
         int count = 0;
-        vector<int> cur = {x};
-
-        while (true) {
-            vector<int> nl;
-            ++count;
-            for (auto e : cur) {
-                if (e < y) {
-                    int t = e * 2;
-                    if (t == y) {
-                        return count;
-                    }
-                    if (hist.find(t) == hist.end()) {
-                        hist.insert(t);
-                        nl.push_back(t);
-                    }
-                }
-                int t = e - 1;
-                if (t == y) {
-                    return count;
-                }
-                if (t > 0 && hist.find(t) == hist.end()) {
-                    hist.insert(t);
-                    nl.push_back(t);
-                }
+        while (y > x) {
+            if (y % 2 == 0) {
+                y /= 2;
+            } else {
+                ++y;
             }
-            cur = nl;
+            ++count;
         }
+        return count + x - y;
     }
 };
 

b7528d67450286c262f91d50e39e6b1c01212f8e
LMJW <heysuperming@gmail.com>
2019-02-17 15:09:52 +1100

    wc124 solved 2

--- wc124/case1.cpp
+++ wc124/case1.cpp
@@ -3,4 +3,50 @@
 
 using namespace std;
 
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        auto p1 = _bfs(root, nullptr, 0, x);
+        auto p2 = _bfs(root, nullptr, 0, y);
+
+        if (p1.first == p2.first && p1.second != p2.second) {
+            return true;
+        }
+        return false;
+    }
+    // pair <depth, parent>
+    pair<int, int> _bfs(TreeNode* cur, TreeNode* p, int depth, int t) {
+        ++depth;
+        auto res = make_pair(depth, -1);
+        if (cur->val == t) {
+            if (p != nullptr) {
+                res.second = p->val;
+            }
+            return res;
+        } else {
+            if (cur->left != nullptr) {
+                res = _bfs(cur->left, cur, depth, t);
+                if (res.second != -1) {
+                    return res;
+                }
+            }
+            if (cur->right != nullptr) {
+                res = _bfs(cur->right, cur, depth, t);
+                if (res.second != -1) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
+
 int main() {}
--- wc124/case2.cpp
+++ wc124/case2.cpp
@@ -5,4 +5,71 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int lx = grid.size();
+        int ly = grid[0].size();
+        int all_oranges = 0;
+        // calculate all oranges
+        int count = 0;  // number of rotaten oranges
+
+        vector<pair<int, int>> r_org;
+        for (int i = 0; i < lx; ++i) {
+            for (int j = 0; j < ly; ++j) {
+                if (grid[i][j] != 0) {
+                    ++all_oranges;
+                    if (grid[i][j] == 2) {
+                        ++count;
+                        r_org.push_back(make_pair(i, j));
+                    }
+                }
+            }
+        }
+        int step = 0;
+        _bfs(grid, lx, ly, count, step, r_org);
+        if (count < all_oranges) {
+            return -1;
+        }
+        return step;
+    }
+    // pair -> steps to roat all orange ; total roted organge
+    void _bfs(vector<vector<int>>& grid,
+              int& gx,
+              int& gy,
+              int& count,
+              int& step,
+              vector<pair<int, int>> start) {
+        vector<pair<int, int>> ns;
+        for (auto it = start.begin(); it != start.end(); ++it) {
+            auto x = it->first;
+            auto y = it->second;
+            if (x + 1 < gx && grid[x + 1][y] == 1) {
+                grid[x + 1][y] = 2;
+                ++count;
+                ns.push_back(make_pair(x + 1, y));
+            }
+            if (x - 1 >= 0 && grid[x - 1][y] == 1) {
+                grid[x - 1][y] = 2;
+                ++count;
+                ns.push_back(make_pair(x - 1, y));
+            }
+            if (y - 1 >= 0 && grid[x][y - 1] == 1) {
+                grid[x][y - 1] = 2;
+                ++count;
+                ns.push_back(make_pair(x, y - 1));
+            }
+            if (y + 1 < gy && grid[x][y + 1] == 1) {
+                grid[x][y + 1] = 2;
+                ++count;
+                ns.push_back(make_pair(x, y + 1));
+            }
+        }
+        ++step;
+        if (ns.size() > 0) {
+            _bfs(grid, gx, gy, count, step, ns);
+        }
+    }
+};
+
 int main() {}

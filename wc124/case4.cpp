6929377398e4e6a2bd1a5d052434f1192eec2b48
LMJW <heysuperming@gmail.com>
2019-02-18 20:41:41 +1100

    cleaner code, but did not consider the fact that same number will cause repeated calculation

--- wc124/case4.cpp
+++ wc124/case4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
 
@@ -36,108 +36,56 @@ struct hash<myhashvec> {
 class Solution {
 public:
     int numSquarefulPerms(vector<int>& A) {
-        vector<pair<int, int>> spair;
-        unordered_set<int> hash;
+        /// @param used stores the index of the used values
+        /// @param store stores all the possible valid combination
         unordered_set<myhashvec> store;
+
         for (int i = 0; i < A.size(); ++i) {
-            for(int j = i+1; j<A.size();++j){
-                int x = A[i]+A[j];
-                auto t = sqrt(x);
-                if (int(t) * int(t) == x) {
-                    spair.push_back(make_pair(A[i], A[j]));
-                    hash.insert(A[i]);
-                    hash.insert(A[j]);
-                }
-            }
+            unordered_set<int> used;
+            used.insert(i);
+            vector<int> tmp;
+            tmp.push_back(A[i]);
+            _dfs(tmp, store, used, A);
         }
 
-        for (auto it = hash.begin(); it != hash.end(); ++it) {
-            vector<int> tmp = vector<int>{*it};
-            _dfs(tmp, store, spair, A.size());
-        }
-
-        int res_count = 0;
         /// ugly solution
-        unordered_map<int,int> hash_a;
-        for(int i =0;i<A.size();++i){
-            if(hash_a.find(A[i]) == hash_a.end()){
-                hash_a[A[i]]=1;
-            }else{
-                ++hash_a[A[i]];
-            }
-        }
-
-
-        for(auto it = store.begin(); it !=store.end();++it){
-            auto tmp = *it;
-            if (tmp.v.size()!=A.size()){
-                continue;
-            }else if(tmp.v.size() == A.size()){
-                bool all_same = true;
-                unordered_map<int,int> hash_tmp;
-                for(int i =0;i<tmp.v.size();++i){
-                    if(hash_tmp.find(tmp.v[i]) == hash_tmp.end()){
-                        hash_tmp[tmp.v[i]]=1;
-                    }else{
-                        ++hash_tmp[tmp.v[i]];
-                    }
-                }
-
-                for(auto it = hash_tmp.begin(); it!=hash_tmp.end();++it){
-                    auto key = it->first;
-                    auto val = it->second;
-                    if (val != hash_a[key]){
-                        all_same = false;
-                        break;
-                    }
-                }
-                if(all_same){
-                    ++res_count;
-                }
-            }
-        }
-        return  res_count;
+        /// ugly solution wouldn't work
+        return store.size();
     }
+
+    /// Calculate whether the sum of two number is sqareful root
+    bool check(int x, int y) {
+        int sum = x + y;
+        auto t = sqrt(sum);
+        return int(t) * int(t) == sum;
+    }
+
     void _dfs(vector<int> path,
-              unordered_set<myhashvec>& st,
-              vector<pair<int, int>> ps,
-              size_t ts) {
+              unordered_set<myhashvec>& store,
+              unordered_set<int> used,
+              const vector<int>& A) {
         /// assume path length > 0
-        auto y = myhashvec(path);
-        if (st.find(y) == st.end()){
-            st.insert(y);
-        }else{
-            return;
-        }
-
-
-        int idx = path.size() - 1;
-        for (int i = 0; i < ps.size(); ++i) {
-            auto a = ps[i].first;
-            auto b = ps[i].second;
-            if (a == path[idx]) {
-                auto np = vector<int>(path);
-                np.push_back(b);
-                auto pss = vector<pair<int, int>>(ps);
-                pss.erase(pss.begin() + i);
-                _dfs(np, st, pss, ts);
+        int last_ele = path.back();
+        if (path.size() < A.size()) {
+            for (int i = 0; i < A.size(); ++i) {
+                unordered_set<int> cpused(used);
+                if (cpused.find(i) == cpused.end() && check(last_ele, A[i])) {
+                    vector<int> nv(path);
+                    nv.push_back(A[i]);
+                    cpused.insert(i);
+                    _dfs(nv, store, cpused, A);
+                }
             }
-            if (b == path[idx] && b != a) {
-                auto np = vector<int>(path);
-                np.push_back(a);
-                auto pss = vector<pair<int, int>>(ps);
-                pss.erase(pss.begin() + i);
-                _dfs(np, st, pss, ts);
-            }
-        }
-        if (path.size() == ts) {
-            st.insert(myhashvec(path));
+        } else if (path.size() == A.size()) {
+            store.insert(myhashvec(path));
+        } else {
+            throw "haha, wrong";
         }
     }
 };
 
 int main() {
-    vector<int> a = vector<int>{0,0,0,0,1,1,1,1};
+    vector<int> a = vector<int>{2, 2, 2, 2, 2, 2, 2, 2};
     auto x = Solution().numSquarefulPerms(a);
     cout << x << endl;
 }
